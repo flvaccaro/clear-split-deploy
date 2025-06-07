a44056c72803e5edce6d65c6fc019e42a557d9ae
Fernando Vaccaro <fernando.vaccaro@despegar.com>
2025-06-07 11:11:50 -0300

    se agrega boton de logout

--- clear-split/src/app/shared/components/header/header.component.ts
+++ clear-split/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../../core/services/theme.service';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  constructor(public theme: ThemeService) {}
+  constructor(public theme: ThemeService, private auth: AuthService) {}
+
+  logout() {
+    this.auth.logout();
+  }
 }
